17a9407afc6ab2812a58485cfd3e8bb4011ee48a
Sereth1 <papakias444@gmail.com>
2025-04-15 21:29:03 +0700

    AA

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -16,6 +16,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 // ✅ Handle OPTIONS request (CORS preflight)
